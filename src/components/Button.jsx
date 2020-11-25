a7bb4706d808bce4214e81aabf6cd0827dcd2d31
ecandotti <enzo.candotti16@hotmail.com>
2020-11-25 03:03:48 +0100

    Add Button Component (...bg)

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const Button = (props) => {
+class Button extends React.Component {
+  render () {
+    const handleSayName = (event) => {
+      this.props.action(event.target.name)
+    }
+
+    const divStyle = {
+      flex: '1',
+      width: '100px',
+      minHeight: '30px'
+    }
+
     return (
-        <button>
-            {props.value}
+      <div>
+        <button style={divStyle} onClick={handleSayName} name={this.props.name}>
+          {this.props.name}
         </button>
+      </div>
     )
+  }
 }
+
+Button.propTypes = {
+  action: PropTypes.func,
+  name: PropTypes.string
+}
+
+export default Button
