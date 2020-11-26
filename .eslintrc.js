b08450769ece7f804af846f27c8b25bccc516762
ecandotti <enzo.candotti16@hotmail.com>
2020-11-26 22:37:35 +0100

    Fix 'ESLint need to know React verison'

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,21 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ['standard', 'plugin:react/recommended'],
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
   parserOptions: {
     ecmaVersion: 2020
+  },
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React',
+      fragment: 'Fragment',
+      version: 'detect',
+      flowVersion: '0.53'
+    }
   }
 }
