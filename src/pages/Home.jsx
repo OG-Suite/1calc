9c9b3874dece6b6d72ae80bb32403da88a1f0d76
ecandotti <enzo.candotti16@hotmail.com>
2020-11-29 00:33:08 +0100

    remplace pad/function code by keyboard component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
-import { Button } from '../components/Button'
+import { KeyBoard } from '../components/KeyBoard'
 import { DarkButton } from '../components/DarkButton'
 import { Screen } from '../components/Screen'
 
 class Home extends React.Component {
   render () {
+    // DarkMode default
+    document.querySelector('html').classList.add('dark')
+
     return (
-      <div className='flex flex-col flex-no-wrap dark:text-white h-screen'>
+      <div className='flex flex-col flex-no-wrap text-gray-800 dark:text-gray-200 h-screen bg-gray-100'>
         <DarkButton />
         <div className='flex-1 flex flex-col flex-nowrap'>
           <Screen />
-          <Button fonction />
-          <Button pad/>
+          <KeyBoard />
         </div>
       </div>
     )
