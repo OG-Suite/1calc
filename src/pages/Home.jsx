c7c90c763b0966e760dc177bfa8310983228aa30
ecandotti <enzo.candotti16@hotmail.com>
2020-12-02 19:19:23 +0100

    delete ShowBar Function cause negrel don't want :(

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { Keyboard } from '../components/Keyboard'
 import { DarkModeButton } from '../components/DarkModeButton'
 import { Screen } from '../components/Screen'
-import { ShowFunc } from '../components/ShowFunc'
 
 class Home extends React.Component {
   render () {
@@ -11,11 +10,10 @@ class Home extends React.Component {
     document.querySelector('html').classList.add('dark')
 
     return (
-      <div className='flex flex-col flex-no-wrap text-gray-800 dark:text-gray-200 h-screen bg-gray-100'>
+      <div className='flex flex-col flex-no-wrap text-gray-800 dark:text-gray-200 h-screen bg-gray-100 duration-1000'>
         <DarkModeButton />
         <div className='flex-1 flex flex-col flex-nowrap'>
           <Screen />
-          <ShowFunc />
           <Keyboard />
         </div>
       </div>
